6db5eea9a39b9f69d08f8f7c3b727fb27dfa8a01
Sebastian Benz <sebastian.benz@gmail.com>
2011-03-13 22:30:39 +0100

    initializing parent references after linking

--- de.sebastianbenz.task/src/de/sebastianbenz/task/model/RestructuringLinker.java
+++ de.sebastianbenz.task/src/de/sebastianbenz/task/model/RestructuringLinker.java
@@ -5,6 +5,7 @@ import org.eclipse.xtext.diagnostics.IDiagnosticConsumer;
 import org.eclipse.xtext.linking.impl.Linker;
 
 import de.sebastianbenz.task.Container;
+import de.sebastianbenz.task.Content;
 import de.sebastianbenz.task.TaskModel;
 
 public class RestructuringLinker extends Linker {
@@ -14,6 +15,9 @@ public class RestructuringLinker extends Linker {
 			IDiagnosticConsumer diagnosticsConsumer) {
 		if (model instanceof TaskModel) {
 			((Container)model).getChildren(); // initialize the references
+		}else if (model instanceof Content) {
+			Content content = (Content) model;
+			content.getParent(); // initialize the parent
 		}
 	}
 

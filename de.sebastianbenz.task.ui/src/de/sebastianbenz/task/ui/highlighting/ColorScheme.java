7f72f7e981dec3c9d884c73769eec19835fbbfeb
Sebastian Benz <sebastian.benz@bmw-carit.de>
2011-07-29 09:05:05 +0200

    changed default color scheme

--- de.sebastianbenz.task.ui/src/de/sebastianbenz/task/ui/highlighting/ColorScheme.java
+++ de.sebastianbenz.task.ui/src/de/sebastianbenz/task/ui/highlighting/ColorScheme.java
@@ -6,12 +6,12 @@ public class ColorScheme {
 
 	// Default
 	public static final RGB BACKGROUND = new RGB(245,  245,  245);
-	public static final RGB CODE = new RGB(101, 123, 131);
-	public static final RGB ANNOTATION = new RGB(38, 139, 210);
-	public static final RGB KEYWORD = new RGB(181, 137, 0);
-	public static final RGB NUMBER = new RGB(220,  50,  47);
-	public static final RGB STRING = new RGB(42, 161, 152);
-	public static final RGB COMMENT = new RGB(147, 161, 161);
+	public static final RGB CODE = new RGB(0, 0, 0);
+	public static final RGB ANNOTATION = new RGB(125, 125, 125);
+	public static final RGB KEYWORD = new RGB(127, 0, 85);
+	public static final RGB NUMBER = new RGB(125, 125, 125);
+	public static final RGB STRING = new RGB(42, 0, 255);
+	public static final RGB COMMENT = new RGB(63, 127, 95);
 	
 	// Solarized dark
 //	public static final RGB BACKGROUND = new RGB(0,  43,  54);
@@ -25,6 +25,7 @@ public class ColorScheme {
 	// solarized light
 //	public static final RGB BACKGROUND = new RGB(253,  246,  227);
 //	public static final RGB CODE = new RGB(101, 123, 131);
+//	public static final RGB  CODE  = new RGB(101, 123, 131);
 //	public static final RGB ANNOTATION = new RGB(38, 139, 210);
 //	public static final RGB KEYWORD = new RGB(181, 137, 0);
 //	public static final RGB NUMBER = new RGB(220,  50,  47);
@@ -32,3 +33,4 @@ public class ColorScheme {
 //	public static final RGB COMMENT = new RGB(147, 161, 161);
 	
 }
+ 

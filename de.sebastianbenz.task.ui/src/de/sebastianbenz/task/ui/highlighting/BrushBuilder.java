38e85d288d1f6a0a9503384edf7a1fc0cd002689
Sebastian Benz <sebastian.benz@gmail.com>
2011-07-31 00:57:46 +0200

    same brush can be used for different languages (e.g. c & cpp)

--- de.sebastianbenz.task.ui/src/de/sebastianbenz/task/ui/highlighting/BrushBuilder.java
+++ de.sebastianbenz.task.ui/src/de/sebastianbenz/task/ui/highlighting/BrushBuilder.java
@@ -1,26 +1,32 @@
 package de.sebastianbenz.task.ui.highlighting;
 
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.newArrayListWithExpectedSize;
 import static de.sebastianbenz.task.ui.highlighting.HighlightingConfiguration.CODE_ID;
 import static java.util.Arrays.asList;
 import static org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration.KEYWORD_ID;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import com.google.common.base.Function;
 
 import de.sebastianbenz.task.ui.highlighting.Brush.Mapping;
 
 public class BrushBuilder {
 
-	private final String name;
 	private String keywords = "";
 	private Mapping[] mappings = new Mapping[0];
+	private String[] names;
 
-	public BrushBuilder(String name) {
-		this.name = name;
+	public BrushBuilder(String... names) {
+		this.names = names;
 	}
 
-	public static BrushBuilder lang(String name) {
-		return new BrushBuilder(name);
+	public static BrushBuilder lang(String... names) {
+		return new BrushBuilder(names);
 	}
 
 	public BrushBuilder keywords(String keywords) {
@@ -33,12 +39,18 @@ public class BrushBuilder {
 		return this;
 	}
 
-	public Brush build() {
-		List<Mapping> mappingsList = newArrayListWithExpectedSize(mappings.length + 1);
+	public Collection<Brush> build() {
+		final List<Mapping> mappingsList = newArrayListWithExpectedSize(mappings.length + 1);
 		mappingsList.addAll(asList(mappings));
 		mappingsList.add(Mapping.keywords(keywords).style(KEYWORD_ID));
 		mappingsList.add(Mapping.pattern("'''").style(CODE_ID));
-		return new Brush(name, mappingsList);
+		return newArrayList(transform(Arrays.asList(names), new Function<String, Brush>(){
+
+			public Brush apply(String name) {
+				return new Brush(name, mappingsList);
+			}
+			
+		}));
 	}
 	
 
